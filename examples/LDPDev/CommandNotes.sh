e58ae9b5066651c457d7b5703f1fc931c18f8f3a
khanhln2907 <khanhln2907@gmail.com>
2021-06-13 23:24:45 +0200

    prepare prediction phase

--- examples/LDPDev/CommandNotes.sh
+++ examples/LDPDev/CommandNotes.sh
@@ -1,13 +1,12 @@
 # Update database to pipeline
-python ${your_install_path}/python/fate_flow/fate_flow_client.py -f upload -c upload_data.json
+# python ${your_install_path}/python/fate_flow/fate_flow_client.py -f upload -c upload_data.json
 
 # Init flow
 # Link web: https://fate.readthedocs.io/en/latest/_build_temp/python/fate_client/flow_client/README.html
 #flow init -c /data/projects/fate/python/conf/service_conf.yaml
 flow init --ip 127.0.0.1 --port 9380
 
-
-# Upload with new version
+# Upload for training version
 flow data upload -c ./examples/LDPDev/upload_data_host.json
 flow data upload -c ./examples/LDPDev/upload_data_guest.json
 
@@ -15,5 +14,12 @@ flow data upload -c ./examples/LDPDev/upload_data_guest.json
 # Link web : https://github.com/FederatedAI/FATE/tree/master/examples/dsl/v2/hetero_secureboost
 flow job submit -c ./examples/LDPDev/dsl/v2/hetero_secureboost/test_secureboost_train_binary_conf.json -d ./examples/LDPDev/dsl/v2/hetero_secureboost/test_secureboost_train_dsl.json
 
+# Deploy the trained model
+flow model deploy --model-id guest-9999#host-9998#model --model-version 202106132103219414043 --cpn-list "dataio_0, intersection_0, hetero_secure_boost_0"
+
+# Update data table for prediction
+flow data upload -c ./examples/LDPDev/upload_data_host_predict.json
+flow data upload -c ./examples/LDPDev/upload_data_guest_predict.json
+
 # Submit prediction process
-flow job submit -c ./examples/dsl/v1/hetero_secureboost/test_predict_conf.json 
+flow job submit -c ./examples/LDPDev/dsl/v2/hetero_secureboost/test_predict_conf.json 
